25934a98077d9a75cd68b8a5a59755bdf3a1ad7c
Alejandro Sánchez Roncero <asanchezr@zeleros.com>
2022-05-09 12:18:51 +0200

    bert and pam cleaned

--- Project_python/test_bert.py
+++
@@ -1,8 +0,0 @@
-from bert_serving.client import BertClient
-
-# make a connection with the BERT server using it's ip address; do not give any ip if same computer
-bc = BertClient()
-# get the embedding
-embedding = bc.encode(["I love data science and analytics vidhya."])
-# check the shape of embedding, it should be 1x768
-print(embedding.shape)
